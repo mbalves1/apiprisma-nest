11bb06f03679dc20c1a6cd533f6d25bea6d4900b
Murilo <mbalves1@outlook.com>
2023-10-30 11:12:23 -0300

    [feat]: swagger install

--- src/main.ts
+++ src/main.ts
@@ -5,10 +5,20 @@ import { UnauthorizedInterceptor } from './common/errors/interceptors/unauthoriz
 import { NotFoundInterceptor } from './common/errors/interceptors/NotFound.interceptor';
 import { ConflictInterceptor } from './common/errors/interceptors/conflictError.interceptor';
 import { DatabaseInterceptor } from './common/errors/interceptors/database.interceptor';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 // import { HttpExceptionFilter } from './common/filters/http-exception/http-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Api nest example')
+    .setDescription('The Simple API description')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true, // Remove all unspecified parameters
@@ -21,6 +31,7 @@ async function bootstrap() {
   app.useGlobalInterceptors(new UnauthorizedInterceptor());
   app.useGlobalInterceptors(new NotFoundInterceptor());
   // app.useGlobalFilters(new HttpExceptionFilter());
+
   await app.listen(3000);
 }
 bootstrap();
